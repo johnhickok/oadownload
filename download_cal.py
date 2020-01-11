3dbfbe054bfd47fd07f09c72c3eb5c552ca28089
John Hickok <jhickok2011@gmail.com>
2020-01-11 07:20:29 -0800

    updated

--- download_cal.py
+++ download_cal.py
@@ -1,18 +1,24 @@
-# This script iterates through the text file from openaddresses.io
-# http://results.openaddresses.io/state.txt
-# that is edited to include just California data sources, 
+# This script downloads just California data sources from openaddresses.io
 # excluding Los Angeles County
 
 import csv, random, time, urllib.request
 
-with open('state_cal.txt') as csvfile:
+# download state.txt data.openaddresses.io
+url_state_txt = 'http://results.openaddresses.io/state.txt'
+urllib.request.urlretrieve(url_state_txt, 'state.txt')
+
+# iterate through state.txt and download files in California
+with open('state.txt', encoding = 'utf8') as csvfile:
   reader = csv.DictReader(csvfile, delimiter='\t')
   for row in reader:
-    country, state, json = row['source'].split('/')
-    filename = json.split('.')[0] + '.zip'
-    url = row['processed']
-    wait = random.uniform(4.5, 9.0)
-    time.sleep(wait)
-	print('Waiting', wait, 'seconds...')
-    print('Downloading ' + filename + ' ...')
-    urllib.request.urlretrieve(url, filename)
+      if (row['source'][0:5]) == 'us/ca' \
+      and row['processed'] != '' \
+      and row['source'][0:17] != 'us/ca/los_angeles':
+        country, state, json = row['source'].split('/')
+        filename = json.split('.')[0] + '.zip'
+        url = row['processed']
+        wait = random.uniform(4.5, 9.0)
+        time.sleep(wait)
+        print('Waiting', wait, 'seconds...')
+        print('Downloading ' + filename + ' ...')
+        urllib.request.urlretrieve(url, filename)
