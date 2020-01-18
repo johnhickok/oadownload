ce4a2c49d8f86e25c97b661585a5c30beb2e17ae
John Hickok <jhickok2011@gmail.com>
2020-01-18 13:03:15 -0800

    Update my_postgres_credentials.py

--- my_postgres_credentials.py
+++ my_postgres_credentials.py
@@ -1,5 +1,5 @@
 # replace the variables below with your own values
 
-dbname   = 'oa_california2' # your database name
-user     = 'postgres' # your user name
-password = 'postgres' # your user name's passsword
+dbname   = 'replace_with_your_database_name' # your database name
+user     = 'your_postgres_user_name' # your user name
+password = 'your_postgres_password' # your user name's passsword
